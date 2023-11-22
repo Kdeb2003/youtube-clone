f1526b5093114719f50708d3801e54374d443374
Kdeb2003 <87771612+Kdeb2003@users.noreply.github.com>
2023-11-22 19:54:59 +0600

    History.js

--- src/pages/History.js
+++ src/pages/History.js
@@ -1,12 +1,9 @@
-// History.js
-
 import React from 'react';
 
 const History = () => {
   return (
     <div>
       <h2>History Page</h2>
-      {/* Add your history page content here */}
     </div>
   );
 }
