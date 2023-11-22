103421cf66c05e3dd83521bab7d7531ea9f295a4
Kdeb2003 <87771612+Kdeb2003@users.noreply.github.com>
2023-11-22 19:53:23 +0600

    YourVideos.js

--- src/pages/YourVideos.js
+++ src/pages/YourVideos.js
@@ -1,12 +1,9 @@
-// YourVideos.js
-
 import React from 'react';
 
 const YourVideos = () => {
   return (
     <div>
       <h2>Your Videos Page</h2>
-      {/* Add your your videos page content here */}
     </div>
   );
 }
