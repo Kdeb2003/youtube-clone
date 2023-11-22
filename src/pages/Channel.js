82de5d37f7989498749ee087a376a8b0be5f1299
Kdeb2003 <87771612+Kdeb2003@users.noreply.github.com>
2023-11-22 19:55:37 +0600

    Channel.js

--- src/pages/Channel.js
+++ src/pages/Channel.js
@@ -1,12 +1,9 @@
-// Channel.js
-
 import React from 'react';
 
 const Channel = () => {
   return (
     <div>
       <h2>Channel Page</h2>
-      {/* Add your channel page content here */}
     </div>
   );
 }
