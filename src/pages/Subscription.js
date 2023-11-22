3d73bd896955bb94b7c48787b5e3703838262609
Kdeb2003 <87771612+Kdeb2003@users.noreply.github.com>
2023-11-22 19:54:01 +0600

    Subscription.js

--- src/pages/Subscription.js
+++ src/pages/Subscription.js
@@ -1,12 +1,9 @@
-// Subscription.js
-
 import React from 'react';
 
 const Subscription = () => {
   return (
     <div>
       <h2>Subscription Page</h2>
-      {/* Add your subscription page content here */}
     </div>
   );
 }
